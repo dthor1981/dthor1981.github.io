1eb92a80b715047a25d8ebcdb60c0e0d4ae266a4
dthor1981 <dthor1981@gmail.com>
2016-06-01 17:43:57 -0400

    Add files via upload

--- js/d3_custom.js
+++ js/d3_custom.js
@@ -29,4 +29,35 @@
           map.labels();
           window.addEventListener('resize', function() { map.resize(); }); 
           
-  
+  var map2 =
+          new Datamap(
+            {
+              element: document.getElementById('map-container-2'),
+              responsive: true,
+              scope: "usa",
+              fills:{
+          
+                    1: 'green',
+                    2: 'yellow',
+                    3: 'red',
+                    4: 'orange',
+                    5: 'brown'
+               },
+              height:500,
+              width:700,
+                
+            dataUrl: '../data/map2.csv',
+            dataType: 'csv',
+            data: {},
+                
+            geographyConfig: {
+                    highlightBorderColor: '#bada55',
+                    popupTemplate: function(geography, data) {
+                    return '<div class="hoverinfo">' + geography.properties.name +'<br>'+ 'Total Obligated Amount, Including Future Obligations: ' +  data.oblAmt + ' '
+                    },
+            highlightBorderWidth: 3
+            },
+            
+            });
+          map.labels();
+          window.addEventListener('resize', function() { map.resize(); }); 
