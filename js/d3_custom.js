1c5110b5bf9dbf98a477115ab4e1d6a582993285
dthor1981 <dthor1981@gmail.com>
2016-06-01 22:20:07 -0400

    Add files via upload

--- js/d3_custom.js
+++ js/d3_custom.js
@@ -32,7 +32,7 @@
   var map2 =
           new Datamap(
             {
-              element: document.getElementById('map-container-2'),
+              element: document.getElementById('map-container-two'),
               responsive: true,
               scope: "usa",
               fills:{
@@ -53,7 +53,7 @@
             geographyConfig: {
                     highlightBorderColor: '#bada55',
                     popupTemplate: function(geography, data) {
-                    return '<div class="hoverinfo">' + geography.properties.name +'<br>'+ 'Total Obligated Amount, Including Future Obligations: ' +  data.oblAmt + ' '
+                    return '<div class="hoverinfo">' + geography.properties.name +'<br>'+ 'Total Obligated Amount, Including Future Obligations: ' +  data.oblAmtDue + ' '
                     },
             highlightBorderWidth: 3
             },
@@ -61,3 +61,47 @@
             });
           map2.labels();
           window.addEventListener('resize2', function() { map2.resize2(); }); 
+          
+          
+  var map3 =
+          new Datamap(
+            {
+              element: document.getElementById('map-container-three'),
+              responsive: true,
+              scope: "usa",
+              fills:{
+          
+                    1: 'green',
+                    2: 'yellow',
+                    3: 'red',
+                    4: 'orange',
+                    5: 'brown',
+                    6: 'indigo',
+                    7: 'violet',
+                    8: 'blue',
+                    9: 'gray',
+                    10: 'pink'
+               },
+              height:500,
+              width:700,
+                
+            dataUrl: '../data/map3.csv',
+            dataType: 'csv',
+            data: {},
+                
+            geographyConfig: {
+                    highlightBorderColor: '#bada55',
+                    popupTemplate: function(geography, data) {
+                    return '<div class="hoverinfo">' + geography.properties.name +'<br>'+ ' Average Obligated Amount, Including Future Obligations:  ' +  data.oblAmt + ' '
+                    },
+            highlightBorderWidth: 3
+            },
+            
+            });
+          map3.labels();
+          window.addEventListener('resize3', function() { map3.resize3(); }); 
+          
+          
+          
+             
+     
