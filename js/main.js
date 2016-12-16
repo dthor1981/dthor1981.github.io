4301581b092cf29de8a1ee61b907e69ad9a64647
David Thor <dthor1981@gmail.com>
2016-12-15 21:58:40 -0500

    newer committ

--- js/main.js
+++ js/main.js
@@ -3,6 +3,9 @@
 jQuery.noConflict();
 jQuery(document).ready(function (){
     mainMenuScroll();
+    shake("#star");
+    shake("#shield");
+    shake("#brush");
 });
     
 function mainMenuScroll(){
@@ -19,5 +22,16 @@ function mainMenuScroll(){
          jQuery('html,body').animate({scrollTop:dest}, 1000,'swing');
  });   
  }
+
+function shake(el){   
+jQuery(el).hover(innerShake);
+}
+function innerShake(){
+//var star = document.getElementById("star");
+//TweenMax.to(this, 0.1, {x:"+=20", yoyo:true, repeat:1});
+//TweenMax.to(this, 0.1, {x:"-=20", yoyo:true, repeat:-1});
+TweenLite.fromTo(this, 2, { rotation: 20}, { rotation: -20, ease: RoughEase.ease.config({ strength: 5, points: 20,taper:"out", randomize: false}), clearProps: "rotation"});
+}
+
 
 
